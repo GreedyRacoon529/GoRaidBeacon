77833efbc5bfe0cdaf095e27c2a6a4230b5fdc7f
GreedyRacoon529 <getgood90000@gmail.com>
2025-07-24 18:54:16 -0800

    Add files via upload

---
+++ Raid-Website/pokemonData.js
@@ -0,0 +1,43 @@
+const pokemonData = {
+  rayquaza: {
+    name: "Rayquaza",
+    tier: "5",
+    type: ["Dragon", "Flying"],
+    weaknesses: ["Ice", "Rock", "Fairy", "Dragon"],
+    cp: { normal: 2191, boosted: 2739 },
+    icon: "https://img.pokemondb.net/artwork/rayquaza.jpg"
+  },
+  mewtwo: {
+    name: "Mewtwo",
+    tier: "5",
+    type: ["Psychic"],
+    weaknesses: ["Bug", "Ghost", "Dark"],
+    cp: { normal: 2387, boosted: 2984 },
+    icon: "https://img.pokemondb.net/artwork/mewtwo.jpg"
+  },
+  kyogre: {
+    name: "Kyogre",
+    tier: "5",
+    type: ["Water"],
+    weaknesses: ["Electric", "Grass"],
+    cp: { normal: 2328, boosted: 2910 },
+    icon: "https://img.pokemondb.net/artwork/kyogre.jpg"
+  },
+  groudon: {
+    name: "Groudon",
+    tier: "5",
+    type: ["Ground"],
+    weaknesses: ["Water", "Ice", "Grass"],
+    cp: { normal: 2351, boosted: 2939 },
+    icon: "https://img.pokemondb.net/artwork/groudon.jpg"
+  },
+  zapdos: {
+    name: "Zapdos",
+    tier: "5",
+    type: ["Electric", "Flying"],
+    weaknesses: ["Rock", "Ice"],
+    cp: { normal: 2015, boosted: 2519 },
+    icon: "https://img.pokemondb.net/artwork/zapdos.jpg"
+  }
+  // You can expand this with more Pokémon as needed
+};
